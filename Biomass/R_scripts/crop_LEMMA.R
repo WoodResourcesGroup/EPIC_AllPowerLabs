48b05db823c7912c767d9d59d99bb10c30736b54
Carmen Tubbesing <ctubbesing@berkeley.edu>
2016-11-30 16:52:19 -0800

    separated first crop of LEMMA into separate script

---
+++ Biomass/R_scripts/crop_LEMMA.R
@@ -0,0 +1,11 @@
+
+LEMMA <- raster("mr200_2012")
+crs(LEMMA) # 5070. based on what this guys says: http://gis.stackexchange.com/questions/128190/convert-srtext-to-proj4text
+plot(LEMMA) # This is just plotting alias for FCID, forest class identification number, as described here: http://lemma.forestry.oregonstate.edu/data/structure-maps
+extent(LEMMA)
+
+LEMMA <- crop(LEMMA, extent(-2362845, -1627605, 1232145, 2456985)) # Crop LEMMA so it only contains CA
+LEMMA_bu <- LEMMA # backup
+writeRaster(LEMMA, filename = "LEMMA.grd", overwrite = T) # save a backup
+# This creates both a .gri and a .grd file
+# I tried writing the raster in GeoTIFF and IMG formats, but they do not retain attribute information, which is critical

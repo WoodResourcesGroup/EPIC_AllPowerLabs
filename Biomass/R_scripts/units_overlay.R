94d418bbb9bfbb6c3eed9cf9b79145f3c717d02f
Carmen Tubbesing <ctubbesing@berkeley.edu>
2017-01-25 17:02:23 -0700

    overlay

--- Biomass/R_scripts/units_overlay.R
+++ Biomass/R_scripts/units_overlay.R
@@ -3,21 +3,26 @@ library(raster)
 
 ### Open FS units
 
+# First all forests to give a CA overview map
 if( Sys.info()['sysname'] == "Windows" ) {
   setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/")
 } else {
   setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
 }
 
+FS <- readOGR(dsn="tempdir",layer = "FS_CA") 
+plot(FS_CA)
+
+# Then forests of interest
 
 FS <- readOGR(dsn="tempdir",layer = "FS") 
-plot(FS)
+plot(FS, add= T, col="red")
 
 ### Open Mountain Home layer
 
 Mtn_hm <- readOGR(dsn = "Mtn_home", layer = "Mtn_home_new")
-plot(Mtn_hm, add=T)
-
+Mtn_hm <- spTransform(Mtn_hm, crs(FS_CA))
+plot(Mtn_hm, add=T, col="purple", border="purple")
 
 if( Sys.info()['sysname'] == "Windows" ) {
   setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/State_Parks")
@@ -25,11 +30,24 @@ if( Sys.info()['sysname'] == "Windows" ) {
   setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/State_Parks")
 }
 
-
 ### Open State Park layer
 st_p <- readOGR(dsn = "CalStPrks_Geodata_Facilities_Public_2016_04.gdb", layer = "CalStPrks_Geodata_Facilities_Public_2016_04")
 
-### Open National Park Layer
+### Open National Park Layers
+
+if( Sys.info()['sysname'] == "Windows" ) {
+  setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/")
+} else {
+  setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
+}
+
+sequ <- readOGR(dsn = "Boundary_SequoiaNP_20100209", layer = "Boundary_SequoiaNP_20100209")
+sequ <- spTransform(sequ, crs(FS_CA))
+plot(sequ, add=T, col="green") 
+
+kc <- readOGR(dsn = "Boundary_KingsNP_20100209", layer = "Boundary_KingsNP_20100209")
+kc <- spTransform(kc, crs(FS_CA))
+plot(kc, add=T, col="green")
 
 ### Open Results
 

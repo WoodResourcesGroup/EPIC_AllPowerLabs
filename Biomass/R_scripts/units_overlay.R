5e3c99898e68695e321335aac63cfacf96b42ec1
Carmen Tubbesing <ctubbesing@berkeley.edu>
2017-01-30 16:44:12 -0700

    overlay simplified

--- Biomass/R_scripts/units_overlay.R
+++ Biomass/R_scripts/units_overlay.R
@@ -9,41 +9,30 @@ if( Sys.info()['sysname'] == "Windows" ) {
 } else {
   setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
 }
-
 FS_CA <- readOGR(dsn="tempdir",layer = "FS_CA") 
 plot(FS_CA)
 
 # Then forests of interest
-
 FS <- readOGR(dsn="tempdir",layer = "FS") 
 plot(FS, add= T, col="red")
 
 ### Open Mountain Home layer
-
 Mtn_hm <- readOGR(dsn = "Mtn_home", layer = "Mtn_home_new")
 Mtn_hm <- spTransform(Mtn_hm, crs(FS_CA))
-plot(Mtn_hm, add=T, col="purple", border="purple")
 
 ### Open State Park layer
 st_p <- readOGR(dsn = "State_Parks", layer = "two_parks")
 st_p<- spTransform(st_p, crs(FS_CA))
-plot(st_p, add=T, col="blue")
 
 ### Open National Park Layers
-
 sequ <- readOGR(dsn = "Boundary_SequoiaNP_20100209", layer = "Boundary_SequoiaNP_20100209")
 sequ <- spTransform(sequ, crs(FS_CA))
-plot(sequ, add=T, col="green") 
-
 kc <- readOGR(dsn = "Boundary_KingsNP_20100209", layer = "Boundary_KingsNP_20100209")
 kc <- spTransform(kc, crs(FS_CA))
-plot(kc, add=T, col="green")
-
 lnp <- readOGR(dsn = "tempdir", layer = "LNP")
 lnp <- spTransform(lnp, crs(FS_CA))
-plot(lnp)
 
-### Just plots
+### Plot them
 plot(FS_CA)
 plot(FS, add= T, col="purple")
 plot(Mtn_hm, add=T, col="red", border="red")
@@ -54,16 +43,6 @@ plot(lnp, add=T, col="orange")
 
 ### Open Results
 
-# First all forests to give a CA overview map
-if( Sys.info()['sysname'] == "Windows" ) {
-  setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/R Results/")
-} else {
-  setwd("~/Documents/Box Sync/EPIC-Biomass/R Results/")
-}
-
-
-#results_1215 <- readOGR(dsn = "Results_2012-2015", layer = "Results_2012-2015")  # takes a long time 
-result_16 <- readOGR(dsn = "Results_2016", layer = "Results_2016")
 
 ### Crop and mask results once for each unit spdf
 

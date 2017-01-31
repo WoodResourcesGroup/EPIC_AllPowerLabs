faea968a64637f4446b20e0b9b47beb13c8db1d2
Carmen Tubbesing <ctubbesing@berkeley.edu>
2017-01-31 10:41:53 -0700

    added LTMU to LEMMA live calcs

--- Biomass/R_scripts/LEMMA_live_units.R
+++ Biomass/R_scripts/LEMMA_live_units.R
@@ -202,36 +202,112 @@ LEMMA.kc_bu <- LEMMA.kc
 ### Save spatial data frame
 writeOGR(obj=spdf, dsn = "LEMMA_units", layer = "LEMMA_kc", driver = "ESRI Shapefile")
 
-#####################################################################################
-### Calculate live biomass for each management unit
-#####################################################################################
+###############################################################################
+### REPEAT WITH LTMU
+###############################################################################
+
+LEMMA <- raster("LEMMA.gri")
+
 if( Sys.info()['sysname'] == "Windows" ) {
   setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/")
 } else {
   setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
 }
-LEMMA.units <- readOGR(dsn="LEMMA_units", layer = "LEMMA_units_nokc")
-units <- readOGR(dsn = "units", layer="units_nokc")
-units <- spTransform(units, crs(LEMMA.units))
-kc <- readOGR(dsn = "Boundary_KingsNP_20100209", layer = "Boundary_KingsNP_20100209")
-kc <- spTransform(kc, crs(LEMMA.units))
+FS_LTMU <- readOGR(dsn = "tempdir", layer = "FS_LTMU")
+FS_LTMU <- spTransform(FS_LTMU, crs(LEMMA))
 
-### divide up results by management unit
-library(rgeos)
-
+# start timer
 strt<-Sys.time()
-intersect <- gIntersection(units[1,], LEMMA.units, byid=T) # TAKES A WHILE
+inputs = 1:nrow(FS_LTMU) # units[3:8] is all polygons in MH
+LEMMA.LTMU <- foreach(i=inputs, .combine = rbind, .packages = c('raster','rgeos'), .errorhandling="remove") %dopar% {
+  single <- FS_LTMU[i,] # select one polygon
+  clip1 <- crop(LEMMA, extent(single)) # crop LEMMA GLN data to the size of that polygon
+  clip2 <- mask(clip1, single) # fit the cropped LEMMA data to the shape of the polygon
+  pcoords <- cbind(clip2@data@values, coordinates(clip2)) # save the coordinates of each pixel
+  pcoords <- as.data.frame(pcoords)
+  pcoords <- na.omit(pcoords) # get rid of NAs in coordinates table (NAs are from empty cells in box around polygon)
+  Pol.ID <- rep(i, nrow(pcoords)) # create a Polygon ID
+  ext <- extract(clip2, single) # extracts data from the raster - each extracted value is the FIA plot # of the raster cell, which corresponds to detailed data in the attribute table of LEMMA
+  tab <- lapply(ext, table) # creates a table that counts how many of each raster value there are in the polygon
+  s <- sum(tab[[1]]) # Counts total raster cells the polygon - this is different from length(clip2tg) because it doesn't include NAs
+  mat <- as.data.frame(tab)
+  mat2 <- as.data.frame(tab[[1]]/s) # gives fraction of polygon occupied by each plot type. Adds up to 1 for each polygon.
+  mat2 <- merge(mat, mat2, by="Var1") # creates table with FIA plot IDs in polygon, number of each, and relative frequency of each
+  
+  # extract attribute information from LEMMA for each plot number contained in the polygon:
+  L.in.mat <- subset(LEMMA@data@attributes[[1]], LEMMA@data@attributes[[1]][,"ID"] %in% 
+                       mat[,1])[,c("ID","BA_GE_3","BPH_GE_3_CRM","TPH_GE_3","QMD_DOM","TREEPLBA")]
+  
+  ### Attribute meanings from LEMMA GLN:
+  ### BA_GE_3 = basal area of live trees >= 2.5 cm dbh (m^2/ha)
+  ### BPH_GE_3_CRM = Component Ratio Method biomass of all live trees >=2.5 cm dbh (kg/ha)
+  ### TPH_GE_3 = Density of live trees >=2.5 cm dbh (trees/ha)
+  ### QMD_DOM = 	Quadratic mean diameter of all dominant and codominant trees (cm)
+  ### TREEPLBA = Tree species with plurality of basal area
+  
+  merge <- merge(L.in.mat, mat2, by.y = "Var1", by.x = "ID") # merge LEMMA data with polygon data into one table
+  # Find biomass per pixel using biomass per tree and estimated number of trees
+  pmerge <- merge(pcoords, merge, by.x ="V1", by.y = "ID") # pmerge has a line for every pixel
+  # problem here
+  pmerge$relBA <- pmerge$BA_GE_3/sum(pmerge$BA_GE_3) # Create column for % of polygon BA in that pixel. 
+  # Create vectors that are the same length as pmerge to combine into final table:
+  QMD_DOM <- pmerge$QMD_DOM # Find the average of the pixels' quadratic mean diameters 
+  TREEPL <-  pmerge$TREEPLBA # Find the tree species that has a plurality in the most pixels
+  Pol.x <- rep(gCentroid(single)@coords[1], nrow(pmerge)) # Find coordinates of center of polygon
+  Pol.y <- rep(gCentroid(single)@coords[2], nrow(pmerge))
+  Pol.Pixels <- rep(s, nrow(pmerge)) # number of pixels
+  # Estimate biomass of live AND dead trees based on LEMMA values of biomass per pixel:
+  All_BM_kgha <- pmerge$BPH_GE_3_CRM 
+  All_Pol_BM_kgha <- rep(mean(pmerge$BPH_GE_3_CRM),nrow(pmerge)) # Average across pixels
+  THA <- pmerge$TPH_GE_3 
+  
+  # Bring it all together
+  final <- cbind(pmerge$x, pmerge$y,pmerge$relBA, pmerge$V1, Pol.x, Pol.y, 
+                 All_BM_kgha,All_Pol_BM_kgha,THA, QMD_DOM,Pol.ID) 
+  final <- as.data.frame(final)
+  final$All_Pol_NO <- (area(single)/10000)*THA # Estimate total number of trees in the polygon
+  final$All_Pol_BM <- (area(single)/10000)*900*All_Pol_BM_kgha # Estimate total tree biomass in the polygon
+  return(final)
+}
 print(Sys.time()-strt)
 
-plot(units[8,])
-plot(ESP.intersect, add=T, col="pink", pch=".")
+# Create a key for each pixel (row)
+key <- seq(1, nrow(LEMMA.LTMU)) 
+LEMMA.LTMU <- cbind(key, LEMMA.LTMU)
 
-ESP.pts.intersect <- strsplit(dimnames(ESP.intersect@coords)[[1]], " ")
-ESP.pts.intersect.id <- as.numeric(sapply(ESP.pts.intersect,"[[",2))
-ESP.pts.extract <- ESP_16[ESP.pts.intersect.id, ]
-ESP_16 <- subset(ESP_16, ESP_16$key %in% ESP.pts.intersect.id)
-plot(ESP_16, add=T, col="pink", pch=".")
+# Rename variables whose names were lost in the cbind
+names(LEMMA.LTMU)[names(LEMMA.LTMU)=="V1"] <- "x"
+names(LEMMA.LTMU)[names(LEMMA.LTMU)=="V2"] <- "y"
+names(LEMMA.LTMU)[names(LEMMA.LTMU)=="V3"] <- "relBA"
+names(LEMMA.LTMU)[names(LEMMA.LTMU)=="V4"] <- "FIA_ID"
 
-writeOGR(obj=ESP_16, dsn = "Results_2016", layer = "Results_2016_ESP_mask", driver = "ESRI Shapefile")
+### Convert to a spatial data frame
+xy <- LEMMA.LTMU[,c("x","y")]
+spdf <- SpatialPointsDataFrame(coords=xy, data = LEMMA.LTMU, proj4string = crs(LEMMA))
+plot(spdf, pch=".")
+plot(FS_LTMU, add=T, border="orange")
+LEMMA.LTMU_bu <- LEMMA.LTMU
 
+### Save spatial data frame
+writeOGR(obj=spdf, dsn = "LEMMA_units", layer = "LEMMA_LTMU", driver = "ESRI Shapefile")
+
+### Maybe delete pixels with zero biomass according to LEMMA
+LEMMA.LTMU.nonzero <- subset(LEMMA.LTMU, LEMMA.LTMU$All_BM_kgha >0)
+LEMMA.LTMU.spdf.nonzero <- subset(spdf, spdf$All_BM_kgha > 0)
+plot(LEMMA.LTMU.spdf.nonzero, pch=".")
+plot(FS_LTMU, add=T, border="orange")
+
+#####################################################################################
+### Calculate live biomass for each management unit
+#####################################################################################
+
+# Only need to do the below steps if you erased results above
+#if( Sys.info()['sysname'] == "Windows" ) {
+  setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/")
+} else {
+  setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
+}
+#strt<-Sys.time()
+#LEMMA.units <- readOGR(dsn="LEMMA_units", layer = "LEMMA_units_nokc")
+#print(Sys.time()-strt)
 

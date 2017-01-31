3f8eb46dae016734f94e92c3f82ea0e7a3c83156
Carmen Tubbesing <ctubbesing@berkeley.edu>
2017-01-31 10:08:33 -0700

    added LTMU to units_overlay

--- Biomass/R_scripts/units_overlay.R
+++ Biomass/R_scripts/units_overlay.R
@@ -3,43 +3,33 @@ library(raster)
 
 ### Open FS units
 
-# First all forests to give a CA overview map
+# First national forests
+FS <- readOGR(dsn="tempdir",layer = "FS") 
+
+# Then LTMU
 if( Sys.info()['sysname'] == "Windows" ) {
   setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/")
 } else {
   setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
 }
-FS_CA <- readOGR(dsn="tempdir",layer = "FS_CA") 
-plot(FS_CA)
-
-# Then forests of interest
-FS <- readOGR(dsn="tempdir",layer = "FS") 
-plot(FS, add= T, col="red")
+FS_LTMU <- readOGR(dsn="tempdir",layer = "FS_LTMU") 
+FS_LTMU <- spTransform(FS_LTMU, crs(FS))
 
 ### Open Mountain Home layer
 Mtn_hm <- readOGR(dsn = "Mtn_home", layer = "Mtn_home_new")
-Mtn_hm <- spTransform(Mtn_hm, crs(FS_CA))
+Mtn_hm <- spTransform(Mtn_hm, crs(FS))
 
 ### Open State Park layer
 st_p <- readOGR(dsn = "State_Parks", layer = "two_parks")
-st_p<- spTransform(st_p, crs(FS_CA))
+st_p<- spTransform(st_p, crs(FS))
 
 ### Open National Park Layers
 sequ <- readOGR(dsn = "Boundary_SequoiaNP_20100209", layer = "Boundary_SequoiaNP_20100209")
-sequ <- spTransform(sequ, crs(FS_CA))
+sequ <- spTransform(sequ, crs(FS))
 kc <- readOGR(dsn = "Boundary_KingsNP_20100209", layer = "Boundary_KingsNP_20100209")
-kc <- spTransform(kc, crs(FS_CA))
+kc <- spTransform(kc, crs(FS))
 lnp <- readOGR(dsn = "tempdir", layer = "LNP")
-lnp <- spTransform(lnp, crs(FS_CA))
-
-### Plot them
-plot(FS_CA)
-plot(FS, add= T, col="purple")
-plot(Mtn_hm, add=T, col="red", border="red")
-plot(st_p, add=T, col="deeppink2", border="deeppink2")
-plot(sequ, add=T, col="green") 
-plot(kc, add=T, col="green")
-plot(lnp, add=T, col="orange")
+lnp <- spTransform(lnp, crs(FS))
 
 ### Merge units
 units <- union(Mtn_hm, st_p)
@@ -47,36 +37,11 @@ units <- union(units, sequ)
 units <- union(units, FS)
 units <- union(units, lnp)
 units$UNIT <- c(rep("MH",6), "CSP", "ESP", "SQNP", "SNF", "ENF", "LNP")
+# This vector of units does not include KCNP or LTMU
+
+### Check them out
+plot(units, col="green")
+plot(FS_LTMU, add=T, col="orange")
+plot(kc, add=T, col="red")
 
 writeOGR(units, dsn="units", layer="units_nokc", overwrite_layer = T, driver = "ESRI Shapefile")
-
-### Crop and mask results once for each unit spdf
-
-library(sp)
-results_1215_MH <- crop(results_1215, extent(MH))
-results_1215_MH <- spTransform(results_1215_MH, crs(Mtn_hm))
-
-results_1215_FS <- crop(results_1215, extent(FS))
-
-results_1215_kc <- crop(results_1215, extent(kc))
-results_1215_lnp <- crop(results_1215, extent(lnp))
-
-### Trying with gIntersect
-
-# First find which points in results fall within MH
-MH.intersect <- gIntersection(Mtn_hm, results_1215_MH, byid=T)
-plot(Mtn_hm, add=T, border="orange")
-MH.pts.intersect <- strsplit(dimnames(MH.intersect@coords)[[1]], " ")
-MH.pts.intersect.id <- as.numeric(sapply(MH.pts.intersect,"[[",2))
-MH.pts.extract <- results_1215_MH[MH.pts.intersect.id, ]
-results_1215_MH_ex <- subset(results_1215_MH, results_1215_MH$key %in% MH.pts.intersect.id)
-
-plot(results_1215_MH_ex)
-plot(Mtn_hm, add=T, border="orange")
-
-# Repeat for st_p
-results_1215_SP <- crop(results_1215, extent(st_p))
-results_1215_SP <- spTransform(results_1215_SP, crs(st_p))
-
-
-

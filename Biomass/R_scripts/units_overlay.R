920c8868fac00512bd200683a7f5988719dc80c3
Natalie <Natalie>
2017-02-02 18:13:56 -0800

    updated README

--- Biomass/R_scripts/units_overlay.R
+++ Biomass/R_scripts/units_overlay.R
@@ -1,21 +1,19 @@
+EPIC <- "C:/Users/Battles Lab/Box Sync/EPIC-Biomass" # Define where your EPIC-BIOMASS folder is located in Box Sync
+#EPIC <- "C:/Users/Carmen/Box Sync/EPIC-Biomass"
+
 library(rgdal)
 library(raster)
 
 ### Open FS units
 
 # First national forests
-FS <- readOGR(dsn="tempdir",layer = "FS") 
-
+setwd(paste(EPIC, "/GIS Data/tempdir", sep=""))
+load(file="FS.Rdata")
 # Then LTMU
-if( Sys.info()['sysname'] == "Windows" ) {
-  setwd("C:/Users/Carmen/Box Sync/EPIC-Biomass/GIS Data/")
-} else {
-  setwd("~/Documents/Box Sync/EPIC-Biomass/GIS Data/")
-}
-FS_LTMU <- readOGR(dsn="tempdir",layer = "FS_LTMU") 
-FS_LTMU <- spTransform(FS_LTMU, crs(FS))
+load(file="FS_LTMU.Rdata")
 
 ### Open Mountain Home layer
+setwd(paste(EPIC, "/GIS Data", sep=""))
 Mtn_hm <- readOGR(dsn = "Mtn_home", layer = "Mtn_home_new")
 Mtn_hm <- spTransform(Mtn_hm, crs(FS))
 
@@ -27,15 +25,20 @@ st_p<- spTransform(st_p, crs(FS))
 sequ <- readOGR(dsn = "Boundary_SequoiaNP_20100209", layer = "Boundary_SequoiaNP_20100209")
 sequ <- spTransform(sequ, crs(FS))
 kc <- readOGR(dsn = "Boundary_KingsNP_20100209", layer = "Boundary_KingsNP_20100209")
-kc <- spTransform(kc, crs(FS))
-lnp <- readOGR(dsn = "tempdir", layer = "LNP")
-lnp <- spTransform(lnp, crs(FS))
+
+KCNP <- spTransform(kc, crs(FS))
+setwd(paste(EPIC, "/GIS Data/units", sep=""))
+save(KCNP, file="KCNP.Rdata")
+
+setwd(paste(EPIC, "/GIS Data/tempdir", sep=""))
+load(file="LNP.Rdata")
+LNP <- spTransform(LNP, crs(FS))
 
 ### Merge units
 units <- union(Mtn_hm, st_p)
 units <- union(units, sequ)
 units <- union(units, FS)
-units <- union(units, lnp)
+units <- union(units, LNP)
 units$UNIT <- c(rep("MH",6), "CSP", "ESP", "SQNP", "SNF", "ENF", "LNP")
 # This vector of units does not include KCNP or LTMU
 
@@ -44,4 +47,5 @@ plot(units, col="green")
 plot(FS_LTMU, add=T, col="orange")
 plot(kc, add=T, col="red")
 
-writeOGR(units, dsn="units", layer="units_nokc", overwrite_layer = T, driver = "ESRI Shapefile")
+setwd(paste(EPIC, "/GIS Data/units", sep=""))
+save(units, file="units.Rdata")

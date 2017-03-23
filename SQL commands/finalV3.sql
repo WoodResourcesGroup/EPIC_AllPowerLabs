f785cec18bbb75db05906820378efe6656423995
Jose Daniel Lara <jdlara@berkeley.edu>
2017-03-23 11:51:44 -0700

    Fix errors in lemma_joins

--- SQL commands/finalV3.sql
+++ SQL commands/finalV3.sql
@@ -2,20 +2,22 @@
 set search_path  = lemmav2, public;
 drop table lemma_total;
 create table lemma_total as
-select x, y, key, "Pol.ID", "Pol.Shap_Ar", array_agg("RPT_YR") as year, max("QMD_DOM") as qmd_dom, 
-  max("BPH_abs") as total_bm_2012, max("TPH_GE_25")/2.47105 as live_trees_acre, max("relNO")/0.222395 as dead_trees_acre, 
+select x, y, key, "Pol.ID", "Pol.Shap_Ar", array_agg("RPT_YR") as year, max("QMD_DOM") as qmd_dom, "TREEPLBA" as common_species,
+  max("BPH_abs") as total_bm_2012, max("TPH_GE_25")/2.47105 as live_trees_acre_lemma, max("relNO")/0.222395 as dead_trees_acre, 
   sum("D_BM_kg") as "D_BM_kg", sum("D_BM_kg") > max("BPH_abs") as truncation, array_agg(trunc) trunc_hist,
 count(*), geom
 from (select * from lemma_1215 union select * from lemma_2016) as foo where trunc != 1
 group by x, y, key, geom, "Pol.ID", "Pol.Shap_Ar"
+having count(*) > 1
 order by count(*) desc;
 INSERT INTO lemma_total (
   select x, y, key, "Pol.ID", "Pol.Shap_Ar", array_agg("RPT_YR") as year, max("QMD_DOM") as qmd_dom, 
-    max("BPH_abs") as total_bm_2012, max("TPH_GE_25")/2.47105 as live_trees_acre, max("relNO")/0.222395 as dead_trees_acre, 
+    max("BPH_abs") as total_bm_2012, max("TPH_GE_25")/2.47105 as live_trees_acre_lemma, max("relNO")/0.222395 as dead_trees_acre, 
     max("D_BM_kg") as "D_BM_kg", sum("D_BM_kg") >= max("BPH_abs") as truncation, array_agg(trunc) trunc_hist,
   count(*), geom
   from (select * from lemma_1215 union select * from lemma_2016) as foo where trunc = 1
   group by x, y, key, geom, "Pol.ID", "Pol.Shap_Ar"
+  having count(*) > 1
   order by count(*) desc);
 
 -- obtain a portion of the data 
@@ -32,6 +34,7 @@ FROM
 (SELECT DISTINCT "Pol.ID" AS pol_id, "Pol.Shap_Ar" AS pol_area FROM lemmav2.lemma_1215
 ORDER BY "Pol.ID") AS pol
 ORDER BY pol_id;	
+
 
 -- Query to obtain the abnormal/normal polygons 
 

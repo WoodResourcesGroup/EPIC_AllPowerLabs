bcb6afbf7f4da2e343985004388ad19256f4013d
Jose Daniel Lara <jdlara@berkeley.edy>
2017-11-21 21:39:59 -0800

    typo fixed

--- Allocation model/google_data.py
+++ Allocation model/google_data.py
@@ -55,34 +55,33 @@ def matching():
     
     try:
         matrx_distance = gmaps.distance_matrix(biomass_coord[0][0], substation_coord[0][0], mode="driving", departure_time="now", traffic_model="pessimistic")
+        error = matrx_distance['rows'][0]['elements'][0]['status']
+        if error != 'OK':
+            try:
+                db_str_e = ('UPDATE lemmav2.substation_routes set api_distance = -99, api_time = -99 where landing_no =' + str(biomass_coord[0][1]) +' and '+ 'feeder_no =' + str(substation_coord[0][1]) +';') 
+                db_engine.execute(db_str_e)
+                db_engine.dispose()
+            except:
+                print('db_write_error_not_ok')
+                pass
+        else:
+            distance = (matrx_distance['rows'][0]['elements'][0]['distance']['value'])
+            try:
+                time = (1 / 3600) * (matrx_distance['rows'][0]['elements'][0]['duration_in_traffic']['value'])
+            except KeyError:
+                time = (1 / 3600) * (matrx_distance['rows'][0]['elements'][0]['duration']['value'])
+                print("KeyError")
+                pass
+            db_str = ('UPDATE lemmav2.substation_routes set api_distance =' + str(distance)+','+ 'api_time = '+ str(time) + ' where landing_no =' + str(biomass_coord[0][1]) +' and '+ 'feeder_no =' + str(substation_coord[0][1]) +';') 
+            try:
+                db_engine.execute(db_str)
+                db_engine.dispose()
+            except:
+                print('db_write_error_ok')
+                pass 
     except:
         print('Api error')
         pass
-
-    error = matrx_distance['rows'][0]['elements'][0]['status']
-    if error != 'OK':
-        try:
-            db_str_e = ('UPDATE lemmav2.substation_routes set api_distance = -99, api_time = -99 where landing_no =' + str(biomass_coord[0][1]) +' and '+ 'feeder_no =' + str(substation_coord[0][1]) +';') 
-            db_engine.execute(db_str_e)
-            db_engine.dispose()
-        except:
-            print('db_write_error_not_ok')
-            pass
-    else:
-        distance = (matrx_distance['rows'][0]['elements'][0]['distance']['value'])
-        try:
-            time = (1 / 3600) * (matrx_distance['rows'][0]['elements'][0]['duration_in_traffic']['value'])
-        except KeyError:
-            time = (1 / 3600) * (matrx_distance['rows'][0]['elements'][0]['duration']['value'])
-            print("KeyError")
-            pass
-        db_str = ('UPDATE lemmav2.substation_routes set api_distance =' + str(distance)+','+ 'api_time = '+ str(time) + ' where landing_no =' + str(biomass_coord[0][1]) +' and '+ 'feeder_no =' + str(substation_coord[0][1]) +';') 
-        try:
-            db_engine.execute(db_str)
-            db_engine.dispose()
-        except:
-            print('db_write_error_ok')
-            pass           
     
 Parallel(n_jobs = 1)(delayed(matching)() for i in range(1,5))
 
